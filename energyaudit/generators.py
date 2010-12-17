5eb522a2eb5a3fa9cba6c52d4f8f8173c80fa1d1
na <na@na-netbook.(none)>
2010-12-17 14:21:25 -0300

    Version estetica harcodeada. Greenaudit como nombre.

--- energyaudit/generators.py
+++ energyaudit/generators.py
@@ -1,7 +1,9 @@
-
+#!/bin/sh
 from django.shortcuts import render_to_response
 from energyaudit.models import *
 from django.contrib.auth.models import User
+from django.db import models
+
 
 def tabs(request):
         return {'tabs':request.user.is_authenticated()}
@@ -13,35 +15,52 @@ def body(request,sel):
 def link_not_found(request):
 	return ["not_found.html",{}]
 
-def get_score(request):
+def head(request):
+        class rate:
+                color=''
+                rango=0
+                indice=''
+                msg=''
+                i=0
+		pad=0
+
         #obtener el nombre de usuario
         u=User.objects.get(username=request.user.username)
         #obtener los edificios que tiene esos codigos
         e=edificio.objects.filter(user=u)
         variables_informe=dict()
+        variables_informe['current']=e[0]
+
+        var=[0,1,2,3,4,5,6,7,8,9]
+        var_col=['00FF03','50F000','80E501','95E500','CFE500','FFFF00','FFE000','FFCA01','FF8000','FB0200']
+        var_ind=[u'A1',u'A2',u'B1',u'B2',u'C1',u'C2',u'D',u'E',u'F',u'G']
+        var_ran=[u'25',u'50',u'75',u'100',u'125',u'175',u'225',u'300',u'380',u'450']
+        ratings=list()
+        for i in var:
+                temp=rate()
+                temp.color=var_col[i]
+                temp.rango=var_ran[i]
+                temp.indice=var_ind[i]
+                temp.i=(i+1)*3
+                if var_ran[i]==variables_informe['current'].rating_potencial:
+                        temp.msg='Indice Potencial: '
+			temp.pad="6"
+                if var_ran[i]==variables_informe['current'].rating_actual:
+                        temp.msg='Indice Actual: '
+                ratings.append(temp)
+
         variables_informe['tabs']=tabs(request)
-        variables_informe['barra']=['FB0200','FF8000','FFCA01','FFE000','FFFF00','CFE500','95E500','80E501','50F000','00FF03']
-	variables_informe['score']=['','','','','','Indice Actual','','Indice Potencial','','']
-	variables_informe['average']=['','','','','','','','','','']
-	variables_informe['edificios']=e
+        variables_informe['edificios']=e
+        variables_informe['ratings']=ratings
+	return variables_informe
+
+
+def get_score(request):
+	variables_informe=head(request)
         return ["scorecard.html",variables_informe]
 
 def get_info(request):
-	#obtener el nombre de usuario
-	u=User.objects.get(username=request.user.username)
-	#obtener los edificios que tiene esos codigos
-	e=edificio.objects.filter(user=u)
-	variables_informe=dict()
-	variables_informe['tabs']=tabs(request)
-        variables_informe['col1']='Info'
-        variables_informe['edificios']=e
-        variables_informe['intro']='get_info'
-        variables_informe['proyeccion_consumo_anual']=''
-        variables_informe['proyeccion_consumo_mensual']=''
-        variables_informe['consumo_por_ambiente']=''
-        variables_informe['fuera_de_horario']=''
-        variables_informe['conclusiones']=''
-        variables_informe['sponsors']='Estos son los Sponsors'
+	variables_informe=head(request)
 	return ["informe.html",variables_informe]
 
 def graficos(request,tipo):
--- energyaudit/models.py
+++ energyaudit/models.py
@@ -7,27 +7,30 @@ class cliente(models.Model):
 	codigo=models.CharField(max_length=5,primary_key=True)
 	raz_social=models.CharField(max_length=30)
 	cuit=models.CharField(max_length=12)
-	
 	def __unicode__(self):
 	        return self.codigo
 
 class edificio(models.Model):
+	rating_choices=((u'25',u'A1'),(u'50',u'A2'),(u'75',u'B1'),(u'100',u'B2'),(u'125',u'C1'),(u'175',u'C2'),(u'225',u'D'),(u'300',u'E'),(u'380',u'F'),(u'450',u'G'))
 	user=models.ForeignKey(User, unique=True) #link a la estructura de validacion de usuario
         codigo=models.ForeignKey(cliente,primary_key=True)
         nombre_edif=models.CharField(max_length=30)
         pais=models.CharField(max_length=30)
         ciudad=models.CharField(max_length=30)
-        latitud=models.FloatField()
-        longitd=models.FloatField()
-        altitud=models.FloatField()
+        latitud=models.FloatField(blank=True, null=True)
+        longitd=models.FloatField(blank=True, null=True)
+        altitud=models.FloatField(blank=True, null=True)
         direccion=models.CharField(max_length=30)
+	actual=models.FloatField(blank=True, null=True)
+	potencial=models.FloatField(blank=True, null=True)
+	rating_actual=models.CharField(max_length=3,choices=rating_choices)
+	rating_potencial=models.CharField(max_length=3,choices=rating_choices)
         def __unicode__(self):
                 return self.nombre_edif
 
 class ambiente(models.Model):
 	tipo_amb_choices=((u'O',u'Oficina'),(u'D',u'Datacenter'),(u'P',u'Pasillo'),(u'R',u'Recepcion'),(u'C',u'Cocina'),(u'S',u'Sala'),(u'd',u'Deposito'))
 	masa_termica_choices=((u'G',u'Grande'),(u'M',u'Mediana'),(u'P',u'Pequena'))
-
         nombre_edif=models.ForeignKey(edificio)
 	fecha_relev=models.DateField()						#fecha relevamiento
 	nombre_amb=models.CharField(max_length=30)				#nombre del ambiente, descriptivo
@@ -46,25 +49,25 @@ class ambiente(models.Model):
 	hora_fin=models.TimeField()
 	hora_inicio_domingo=models.TimeField()
 	hora_fin_domingo=models.TimeField()
-	personal_permanente=models.PositiveSmallIntegerField()
+	personal_permanente=models.PositiveSmallIntegerField(null=True)
 	def __unicode__(self):
 		return self.nombre_amb
 
 class pared(models.Model):
 	orientacion_choices=((u'N',u'Norte'),(u'S',u'Sur'),(u'E',u'Este'),(u'O',u'Oeste'))
 	exterior_choices=((u'A',u'Intemperie, alta exposicion'),(u'B',u'Intemperie, baja exposicion'),(u'I',u'Interior'))
-	tipo_vidrio_choices=((u's',u'Vidrio simple sin aislar'),(u's',u'Vidrio simple aislado'),(u'D','DVH')) 
+	tipo_vidrio_choices=((0.8,u'Vidrio simple'),(0.6,u'Vidrio Doble'),(0.5,'Triple')) 
+	coating_vidrio_choices=((0.8,u'Transparente'),(0.6,u'Baja emisividad'),(0.5,'Polarizado')) 
 	tipo_abertura_choices=((u'a',u'Interna, alta perdida'),(u'b',u'Interna, baja perdida'),(u'A',u'Externa, alta perdida'),(u'B',u'Externa, baja perdida'))
-
-
+	c_pared_choices=((50,u'Acero'),(0.04,u'Corcho'),(0.163,'Goma'),(72,u'Hierro'),(1.4,'Hormigon'),(0.66,u'Ladrillo de mamposteria'),(0.8,'Ladrillo comun'),(0.04,u'Lana de vidrio'),(0.13,'Madera'),(2.09,'Marmol'),(0.19,u'Plexiglas'),(0.76,'Tejas ceramicas'),(1,u'Baja aislacion'),(0.5,u'Media aislacion'),(0.2,u'Alta aislacion')) 
 	ambiente_pared=models.ForeignKey(ambiente)
 	orientacion_pared=models.CharField(max_length=1,choices=orientacion_choices)
 	area_pared=models.FloatField()
-	r_pared=models.FloatField()
+	tipo_pared=models.FloatField(choices=c_pared_choices)
 	exterior=models.CharField(max_length=1,choices=exterior_choices)
 	area_vidrio=models.FloatField()
-	tipo_vidrio=models.CharField(max_length=1,choices=tipo_vidrio_choices)
-	r_vidrio=models.FloatField()
+	tipo_vidrio=models.FloatField(choices=tipo_vidrio_choices)
+	coating_vidrio=models.FloatField(choices=coating_vidrio_choices)
 	area_abertura=models.FloatField()
 	tipo_abertura=models.CharField(max_length=1,choices=tipo_abertura_choices)
         def __unicode__(self):
@@ -74,11 +77,10 @@ class pared(models.Model):
 class artefacto(models.Model):
 	tipo_artefacto_choices=((u'B',u'Bajo consumo'),(u'T',u'Tubo de luz'),(u'D',u'Dicroica'),(u'I','Incandescente'),(u'C',u'Computadora'),(u'i',u'Impresora'),(u'P',u'Proyector'),(u'H',u'Heladera'),(u'a',u'Cocina electrica'),(u'E',u'Estufa electrica'),(u'O',u'Otro'))
 	ciclo_activo_choices=((u'E',u'Siempre activo'),(u'L',u'Activo horario laboral'),(u'S',u'Activo semi-optimo'),(u'O',u'Activo optimo'),(u'N',u'Activo horario NO laboral'))
-
 	ambiente_artefacto=models.ForeignKey(ambiente)
 	tipo_artefacto=models.CharField(max_length=1,choices=tipo_artefacto_choices)
 	potencia_activo=models.FloatField()
-        potencia_stand_by=models.FloatField()
+        potencia_stand_by=models.FloatField(blank=True, null=True)
 	ciclo_activo=models.CharField(max_length=1,choices=ciclo_activo_choices)	
 	temp_funcionamiento=models.FloatField()
         def __unicode__(self):
